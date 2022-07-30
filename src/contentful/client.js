95af14b10214b873f75d36344851ed33c01939f3
al fahim bin faruk <fmd03700@gmail.com>
2022-07-31 00:37:13 +0600

    private value removed

--- src/contentful/client.js
+++ src/contentful/client.js
@@ -1,8 +1,8 @@
 import * as contentful from "contentful";
 
 const client = contentful.createClient({
-  space: "d2buf511rn5e",
-  accessToken: "rVjWmsegZdSM_KMZeUhGH4jpecN6qhARWum4joxnBC8",
-  host: "cdn.contentful.com",
+  space: process.env.REACT_APP_CONTENTFUL_SPACE,
+  accessToken: process.env.REACT_APP_CONTENTFUL_PUBLISHED_ACC_TOKEN,
+  host: process.env.REACT_APP_CONTENTFUL_HOST,
 });
 export default client;
